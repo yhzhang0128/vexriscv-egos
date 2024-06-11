6680406763e1401b538fc86386177ee5408b1f34
Yunhao Zhang <yz2327@cornell.edu>
2024-06-10 23:17:32 -0500

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -1,2 +1,2 @@
-echo "Press the reset button on the board!"
+echo "You may need to press the reset button on the board."
 litex/litex/litex/tools/litex_term.py --safe --images=linux/boot.json /dev/ttyUSB1
