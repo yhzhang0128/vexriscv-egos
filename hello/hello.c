3bd46ff8f58df19fb6934abca7e5bc0d175bbf05
Yacqub Mohamed <yam22@cornell.edu>
2024-06-19 17:50:55 -0400

    Working Input/Output UART Implementation; Read Name; Print Name

--- hello/hello.c
+++ hello/hello.c
@@ -1,6 +1,11 @@
-#define UART_BASE       0xF0001000
-#define UART_OFF_RXTX   0x00
-#define UART_OFF_TXFULL 0x04
+#define UART_BASE        0xF0001000
+#define UART_OFF_RXTX    0x00
+#define UART_OFF_TXFULL  0x04
+#define UART_OFF_RXEMPTY 0x08
+#define UART_OFF_EVPEND  0x10
+
+#define UART_TX 0x01
+#define UART_RX 0x10
 
 #define LB(base, off)       (*((char volatile *)(base + off)))
 #define SB(base, off, byte) (*((char volatile *)(base + off)) = byte)
@@ -11,11 +16,38 @@ void putchr(char c) {
   SB(UART_BASE, UART_OFF_RXTX, c);
 }
 
-int main() {
-  const char str[14] = "Hello World!\n";
+char readchr() {
+  while (LB(UART_BASE, UART_OFF_RXEMPTY))
+    ;
 
-  for (int i = 0; i < 14; i++)
+  char byte = LB(UART_BASE, UART_OFF_RXTX);
+  
+  /* Clearing Pending Bit Pops Character off Rx Queue */
+  SB(UART_BASE, UART_OFF_EVPEND, ~UART_RX);
+
+  return byte;
+}
+
+void printstr(char *str, int len) {
+  for (int i = 0; i < len; i++)
     putchr(str[i]);
+}
 
+int main() {
+  char str1[] = "Input Name: ";
+  char buff[100] = "Hello ";
+
+  printstr(str1, 12);
+
+  int len = 6;
+  while ((buff[len] = readchr()) != '\n') {
+    putchr(buff[len]);
+    len++;
+  }
+
+  putchr('\r');
+  putchr('\n');
+
+  printstr(buff, len);
   return 0;
 }
